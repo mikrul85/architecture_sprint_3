d8b21ade56079f38b937a1077d194e69c8bf11aa
Михаил <mihail@MacBook-Pro-Mihail-2.local>
2025-03-28 02:03:14 +0300

    Выполнено задание 1 и схема 1 задания 2

--- smart-home-monolith/src/main/java/ru/yandex/practicum/smarthome/service/HeatingSystemService.java
+++ smart-home-monolith/src/main/java/ru/yandex/practicum/smarthome/service/HeatingSystemService.java
@@ -4,10 +4,16 @@ import ru.yandex.practicum.smarthome.dto.HeatingSystemDto;
 import ru.yandex.practicum.smarthome.entity.HeatingSystem;
 
 public interface HeatingSystemService {
+    /// Получить состояние системы
     HeatingSystemDto getHeatingSystem(Long id);
+    /// Обновить состояние системы
     HeatingSystemDto updateHeatingSystem(Long id, HeatingSystemDto heatingSystemDto);
+    /// Включить отопление
     void turnOn(Long id);
+    /// Выключить отопление
     void turnOff(Long id);
+    /// Установить температуру
     void setTargetTemperature(Long id, double temperature);
+    /// Получить температуру
     Double getCurrentTemperature(Long id);
 }
